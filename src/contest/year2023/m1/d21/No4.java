77cdaa157d18cd1fcb1da90d3fe0ce93d448ba46
wangjunshen <1476105441@qq.com>
2023-01-26 00:30:02 +0800

    第96场双周赛 t4

--- src/contest/year2023/m1/d21/No4.java
+++ src/contest/year2023/m1/d21/No4.java
@@ -2,6 +2,18 @@ package contest.year2023.m1.d21;
 
 public class No4 {
     public boolean isReachable(int targetX, int targetY) {
-        return false;
+        int gcd = gcd(targetX, targetY);
+        while (gcd % 2 == 0) {
+            gcd /= 2;
+        }
+        return gcd == 1;
+    }
+    private int gcd(int x,int y){
+        while (y > 0) {
+            int temp = y;
+            y = x % y;
+            x = temp;
+        }
+        return x;
     }
 }
