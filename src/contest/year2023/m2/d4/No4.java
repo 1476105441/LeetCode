bbe3ad37833f600da22b5a856dc869729893fd3a
wangjunshen <1476105441@qq.com>
2023-02-16 09:54:10 +0800

    第331场周赛 & 第97场双周赛 t4

--- src/contest/year2023/m2/d4/No4.java
+++ src/contest/year2023/m2/d4/No4.java
@@ -1,7 +1,8 @@
 package contest.year2023.m2.d4;
 
 public class No4 {
-    public boolean isPossibleToCutPath(int[][] grid) {
+    //失败，比赛中没有写出来
+    /*public boolean isPossibleToCutPath(int[][] grid) {
         int m = grid.length,n = grid[0].length;
         int[][] dp = new int[m][n];
         dp[0][0] = 1;
@@ -19,5 +20,89 @@ public class No4 {
             }
         }
         return dp[m-2][n-1] == 0 || dp[m-1][n-2] == 0;
+    }*/
+
+    //两个回溯，1ms
+    /*int[][] set;
+    boolean res;
+    public boolean isPossibleToCutPath(int[][] grid) {
+        int m = grid.length,n = grid[0].length;
+        res = false;
+        set = new int[m][n];
+        res = !dfs1(0,0,grid);
+        set[0][0] = 0;
+        dfs2(0,0,grid);
+        return res;
+    }
+    private boolean dfs1(int i,int j,int[][] grid){
+        if (i >= grid.length || j >= grid[0].length) {
+            return false;
+        }
+        if (i == grid.length - 1 && j == grid[0].length - 1) {
+            return true;
+        }
+        if (grid[i][j] == 0) {
+            return false;
+        }
+        set[i][j] = 1;
+        //往下走
+        boolean flag = dfs1(i + 1, j, grid);
+        if (!flag) {
+            flag = dfs1(i,j+1,grid);
+        }
+        if (!flag) {
+            set[i][j] = 0;
+        }
+        return flag;
+    }
+    private boolean dfs2(int i,int j,int[][] grid){
+        if (i >= grid.length || j >= grid[0].length) {
+            return false;
+        }
+        //走到尽头了
+        if (i == grid.length - 1 && j == grid[0].length - 1) {
+            return true;
+        }
+        if (grid[i][j] == 0) {
+            return false;
+        }
+        if (set[i][j] == 1) {
+            res = true;
+            return true;
+        }
+        set[i][j] = 1;
+        //往右走
+        boolean flag = dfs2(i, j + 1, grid);
+        if (!flag) {
+            //往下走
+            flag =  dfs2(i+1,j,grid);
+        }
+        return flag;
+    }*/
+
+    //不需要使用额外的数组存放访问状态，0ms
+    public boolean isPossibleToCutPath(int[][] grid) {
+        int m = grid.length,n = grid[0].length;
+        boolean res = !dfs(0, 0, grid);
+        grid[0][0] = 1;
+        res = res || !dfs(0,0,grid);
+        return res;
+    }
+    public boolean dfs(int i,int j,int[][] grid){
+        if (i >= grid.length || j >= grid[0].length) {
+            return false;
+        }
+        if (i == grid.length - 1 && j == grid[0].length - 1) {
+            return true;
+        }
+        if (grid[i][j] == 0) {
+            return false;
+        }
+        grid[i][j] = 0;
+        boolean b = dfs(i + 1, j, grid);
+        if (!b) {
+            b = dfs(i,j+1,grid);
+        }
+        return b;
     }
 }
--- src/contest/year2023/m2/d5/No4.java
+++ src/contest/year2023/m2/d5/No4.java
@@ -1,5 +1,70 @@
 package contest.year2023.m2.d5;
 
+import java.util.*;
+
 public class No4 {
+    //成功，44ms
+    //最近一段时间自己写出来的困难题！
+    public long minCost(int[] basket1, int[] basket2) {
+        int n = basket1.length;
+        Map<Integer,Integer> map1 = new HashMap<>(256),map2 = new HashMap<>(256);
+        Set<Integer> set = new HashSet<>();
+        //统计两个数组中元素的个数
+        for (int i = 0; i < n; i++) {
+            map1.put(basket1[i],map1.getOrDefault(basket1[i],0)+1);
+            map2.put(basket2[i],map2.getOrDefault(basket2[i],0)+1);
+            //统计数组究竟出现了哪些元素
+            set.add(basket1[i]);
+            set.add(basket2[i]);
+        }
+        long min = 1000000000;
+        List<Integer> list1 = new ArrayList<>(),list2 = new ArrayList<>();
+        for (Integer num : set) {
+            Integer v1 = map1.get(num),v2 = map2.get(num);
+            if (v1 == null) {
+                v1 = 0;
+            }
+            if (v2 == null) {
+                v2 = 0;
+            }
+            int count = v1 + v2;
+            if ((count & 1) == 1) {
+                return -1;
+            }
+            if (v1 > v2) {
+                int temp = (v1 - v2) >> 1;
+                for (int i = 0; i < temp; i++) {
+                    list1.add(num);
+                }
+            } else if (v1 < v2) {
+                int temp = (v2 - v1) >> 1;
+                for (int i = 0; i < temp; i++) {
+                    list2.add(num);
+                }
+            } else {
+                min = Math.min(min,num);
+            }
+        }
+        Collections.sort(list1);
+        Collections.sort(list2);
+        int l1 = 0, l2 = 0, r1 = list1.size() - 1, r2 = r1, c = r1 + 1;
+        long res = 0;
+        while (c > 0) {
+            Integer lv1 = list1.get(l1),lv2 = list2.get(l2);
+            if (lv1 < lv2) {
+                res += Math.min(min << 1,lv1);
+                min = Math.min(min,lv1);
+                l1++;
+                r2--;
+            } else {
+                res += Math.min(min << 1,lv2);
+                min = Math.min(min,lv2);
+                l2++;
+                r1--;
+            }
+            c--;
+        }
+        return res;
+    }
 
 }
