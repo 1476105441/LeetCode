ad8cf086e2f78cf85f3e1cf4292b95af5b63995d
wangjunshen <1476105441@qq.com>
2023-02-24 00:18:58 +0800

    第333场周赛 t4

--- src/contest/year2023/m2/d19/No3.java
+++ src/contest/year2023/m2/d19/No3.java
@@ -1,7 +1,8 @@
 package contest.year2023.m2.d19;
 
 public class No3 {
-    private static int[] primes = new int[]{2,3,5,7,11,13,17,19,23,29};
+    //看了题解之后写出来的，还没有彻底理解
+    /*private static int[] primes = new int[]{2,3,5,7,11,13,17,19,23,29};
     private static int MX = 31,PN = primes.length,mod = 1000000007;
     private static int[] masks = new int[MX];
     static{
@@ -46,5 +47,52 @@ public class No3 {
         //那就是当res == 1000000007的时候，mod了的结果为0，返
         //回res-1就是-1，结果错误
         return (int)((res-1) % mod);
+    }*/
+
+    //解题思路：题目的数据范围非常小，我们可以这点入手。
+    //可以将数字分解为质数的乘积，拥有相同质数的数字不能放到一个子集
+    //中，这样构造出来的子集就是一个无平方子集
+    private static int[] primes = new int[]{2,3,5,7,11,13,17,19,23,29};
+    private static int mx = 31,pn = primes.length,mod = 1000000007;
+    private static int[] masks = new int[mx];
+    static{
+        for (int i = 2; i < mx; i++) {
+            int mask = 0;
+            for (int j = 0; j < pn; j++) {
+                int p = primes[j];
+                if (p > i) {
+                    break;
+                }
+                if (i % p == 0) {
+                    if (i % (p * p) == 0) {
+                        mask = -1;
+                        break;
+                    }
+                    mask = mask | (1 << j);
+                }
+            }
+            masks[i] = mask;
+        }
+    }
+    public int squareFreeSubsets(int[] nums) {
+        int n = nums.length,m = 1 << pn;
+        int[] dp = new int[m];
+        dp[0] = 1;
+        for (int i = 0; i < n; i++) {
+            int mask = masks[nums[i]];
+            if (mask >= 0) {
+                for (int j = m-1; j >= mask; j--) {
+                    //j如果包含mask，则需要更新集合个数
+                    if ((j | mask) == j) {
+                        dp[j] = (dp[j] + dp[j ^ mask]) % mod;
+                    }
+                }
+            }
+        }
+        long res = 0;
+        for (int i = 0; i < m; i++) {
+            res += dp[i];
+        }
+        return (int)((res - 1) % mod);
     }
 }
--- src/contest/year2023/m2/d19/No4.java
+++ src/contest/year2023/m2/d19/No4.java
@@ -1,5 +1,41 @@
 package contest.year2023.m2.d19;
 
 public class No4 {
-
+    //题目要求字典序最小，那么从第一个开始构造的字符逐渐增大，可以
+    //使字典序最小
+    public String findTheString(int[][] lcp) {
+        int m = lcp.length,n = lcp[0].length;
+        char[] chars = new char[n];
+        char c = 'a';
+        //构造
+        for (int i = 0; i < n; i++) {
+            if (chars[i] == 0) {
+                if (c > 'z') {
+                    return "";
+                }
+                chars[i] = c;
+                for (int j = i+1; j < n; j++) {
+                    if (lcp[i][j] > 0) {
+                        chars[j] = c;
+                    }
+                }
+                c++;
+            }
+        }
+        int[][] dp = new int[n+1][n+1];
+        //验证
+        for (int i = n-1; i > -1; i--) {
+            for (int j = n-1; j > -1; j--) {
+                int cnt = 0;
+                if (chars[i] == chars[j]) {
+                    cnt = dp[i+1][j+1] + 1;
+                }
+                if (cnt != lcp[i][j]) {
+                    return "";
+                }
+                dp[i][j] = cnt;
+            }
+        }
+        return new String(chars);
+    }
 }
