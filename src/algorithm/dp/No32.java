6cbc6ada6341ed04b97c480b84809a3ea15fa475
wangjunshen <1476105441@qq.com>
2023-02-24 09:22:48 +0800

    动态规划 No32

--- src/algorithm/dp/No32.java
+++ src/algorithm/dp/No32.java
@@ -1,26 +1,32 @@
 package algorithm.dp;
 
+import java.util.Stack;
+
 public class No32 {
     //最长有效括号
-
+    //栈，成功4ms
     public int longestValidParentheses(String s) {
         char[] chars = s.toCharArray();
         int n = chars.length,res = 0;
-        int l = 0,c = 0;
-        for (int i = 0; i < n; i++) {
-            if (chars[i] == '(') {
-                l++;
+        int[] dp = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0;i < n;i++){
+            if(chars[i] == ')'){
+                if(!stack.isEmpty()){
+                    int loc = stack.pop();
+                    dp[i] = dp[loc] + 2;
+                    if(loc != i-1 && i > 0){
+                        dp[i] += dp[i-1];
+                    }
+                }
             } else {
-                if (l == 0) {
-                    res = Math.max(res, c);
-                    c = 0;
-                } else {
-                    c++;
-                    l--;
+                stack.push(i);
+                if(i > 0 && chars[i-1] == ')'){
+                    dp[i] = dp[i-1];
                 }
             }
+            res = Math.max(res,dp[i]);
         }
-        res = Math.max(res,c);
-        return res << 1;
+        return res;
     }
 }
