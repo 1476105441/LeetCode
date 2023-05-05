4122cc4d7e55f707fb54751997d19e8f0b3c4a34
wangjunshen <1476105441@qq.com>
2023-05-05 08:28:05 +0800

    第339场周赛 No4思路记录

--- src/contest/year2023/m4/d2/No4.java
+++ src/contest/year2023/m4/d2/No4.java
@@ -10,7 +10,7 @@ public class No4 {
     //2、如果n == k，则数组只能反转一次
     //3、bfs
     //4、如何确定每次翻转的位置？
-    public int[] minReverseOperations(int n, int p, int[] banned, int k) {
+    /*public int[] minReverseOperations(int n, int p, int[] banned, int k) {
         int[] res = new int[n];
         Arrays.fill(res,-1);
         res[p] = 0;
@@ -46,5 +46,14 @@ public class No4 {
             dep++;
         }
         return res;
+    }*/
+
+    //等差数列+bfs+平衡树
+    //1、推算出位置i的对称点位的位置，设左边界l，右边界r，对称点为r+l-i
+    //2、推算出当区间变动时，对称点的变动，如果区间向左移动，l需要减一，r也是如此，所以对称点需要减2；同理向右移动对称点需要加2
+    //3、讨论边界问题
+    public int[] minReverseOperations(int n, int p, int[] banned, int k) {
+        TreeSet<Integer> s = new TreeSet<>();
+        return null;
     }
 }
