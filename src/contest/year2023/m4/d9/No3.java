c5137f8e97711e3f8b32ec66c346d1e8eb5e665d
wangjunshen <1476105441@qq.com>
2023-05-10 00:09:46 +0800

    第340场周赛 No3

---
+++ src/contest/year2023/m4/d9/No3.java
@@ -0,0 +1,40 @@
+package contest.year2023.m4.d9;
+
+import java.util.Arrays;
+
+public class No3 {
+    // 2616、最小化数对的最大差值
+
+    //一般这种找出最大值的最小值的题目，可以使用二分法解决
+    //或者贪心
+    public int minimizeMax(int[] nums, int p) {
+        if(p == 0){
+            return 0;
+        }
+        Arrays.sort(nums);
+        int l=0,r=1000000000;
+        while(l < r){
+            int c=((r-l)>>1) + l;
+            if(check(c,p,nums)){
+                r = c;
+            } else{
+                l = c+1;
+            }
+        }
+        return l;
+    }
+    private boolean check(int val,int p,int[] nums){
+        int n = nums.length;
+        for(int i=0;i < n;i++){
+            if(i < n-1 && Math.abs(nums[i] - nums[i+1]) <= val){
+                p--;
+                //跳过下一个元素
+                i++;
+                if(p == 0){
+                    return true;
+                }
+            }
+        }
+        return p == 0;
+    }
+}
